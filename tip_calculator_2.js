5e3315202e68a09a29e77b509ea5fc31114e230d
Nicholas Nam <62817307+n1ckDotEXE@users.noreply.github.com>
2020-04-20 12:25:56 -0400

    Update tip_calculator_2.js

--- tip_calculator_2.js
+++ tip_calculator_2.js
@@ -1,13 +1,13 @@
-function tipAmount(bill,service){
+function billAmount(bill,service){
     if (service === 'good') {
-        return 'The tip is $' + bill * .20
+        return 'The total bill is $' + bill * 1.20
     } else if (service === 'fair') {
-        return 'The tip is $' + bill * .15
+        return 'The total bill is $' + bill * 1.15
     } else if (service === 'bad') {
-        return 'The tip is $' + bill * .10
+        return 'The total bill is $' + bill * 1.10
     } else {
         return 'Enter good, fair, or bad';
     }
 }
 
-console.log(tipAmount(100,'good'));
+console.log(billAmount(100,'good'));
